93c856c3620e8d477b6f352a15aa05863220cb9e
Julean Hommel <juleanhommel@gmail.com>
2021-11-16 13:32:22 +0100

    Add project files.

---
+++ FancyApp/FancyApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using FancyApp.ViewModels;
+using FancyApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FancyApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ FancyApp/FancyApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FancyApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FancyApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
